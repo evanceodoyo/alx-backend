8b591b45b650d813928172794f7705bbf8344a82
Evance Odoyo <odoyoevanc@gmail.com>
2024-05-18 15:59:32 +0300

    simple pagination

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""
+Implementation of a simple pagination.
+"""
+import csv
+from typing import List
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int]:
+    """
+    Function that returns of size two containing a start index and end index
+    corresponding to the range of indexes to return in a list for those
+    particular parameters.
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Gets the page with items of the given size.
+        """
+        try:
+            assert type(page) is int and\
+                page > 0 and\
+                type(page_size) is int and\
+                page_size > 0
+            dataset = self.dataset()
+            dataset_len = len(dataset)
+
+            if page > dataset_len or page_size > dataset_len:
+                return []
+
+            indexes = index_range(page, page_size)
+            return dataset[indexes[0]:indexes[1]]
+
+        except AssertionError as e:
+            raise e
