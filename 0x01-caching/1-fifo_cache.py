30c70f2b4a3383bfddd52580e1b240c70950a8ff
Evance Odoyo <odoyoevanc@gmail.com>
2024-05-22 12:15:04 +0300

    simple FIFO caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Implements a simple FIFO cache.
+"""
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    Defines methods for simple FIFO caching system.
+    """
+    def __init__(self):
+        """
+        Initialize
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        """
+        if key and item:
+            self.cache_data[key] = item
+
+        keys = list(self.cache_data.keys())
+        if len(keys) > BaseCaching.MAX_ITEMS:
+            print(f"DISCARD: {keys[0]}")
+            del self.cache_data[keys[0]]
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        return self.cache_data.get(key)
