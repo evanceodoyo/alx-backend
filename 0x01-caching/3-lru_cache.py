cc156472bbfcef5bde6ecb4d891fad946e8f75cb
Evance Odoyo <odoyoevanc@gmail.com>
2024-05-23 12:16:14 +0300

    Least recently used (LRU) cache

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Implements Least Recently Used (LRU) cache.
+"""
+from collections import OrderedDict
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    Defines methods for LRU caching system.
+    """
+    def __init__(self):
+        """
+        Initialize
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        Add an item in the cache.
+        """
+        if not self.cache_data.get(key):
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(last=False)
+                print(f"DISCARD: {last_key}")
+
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        if self.cache_data.get(key):
+            self.cache_data.move_to_end(key, True)
+        return self.cache_data.get(key)
